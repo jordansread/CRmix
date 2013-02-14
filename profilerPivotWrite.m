77f3dde449441b812a1fb42a04bf83d6ecd55071
Jordan S Read <jread@usgs.gov>
2013-02-13 18:25:35 -0600

    Initial commit

---
+++ profilerPivotWrite.m
@@ -0,0 +1,45 @@
+function profilerPivotWrite
+
+clc
+close all
+fileInput = '2011\2011CrystalLake_SondeTable.dat';
+varN   = 'dosat';
+fileOut   = ['Data/Crystal_2011.' varN];
+
+rootFolder = [getenv('USERPROFILE') '\Desktop\CRmix\Data\'];
+delim  = ',';
+sonVar = '%s %f %f %f %f %f %f %f %f';
+varI = 8;
+zI   = 3;
+zInt = 1;
+tInt = 1/24;    % day frac
+zOff = 0;
+
+%% loop through vars
+
+fID = fopen([rootFolder fileInput]);
+heads= textscan(fID,'%s',(length(sonVar)+1)/3,...
+    'HeaderLines',1,'Delimiter',delim);
+
+heads{1} = regexprep(heads{1}, '"', '');
+disp(['pivoting and writing ' heads{1}{varI} ' to ' fileOut]);
+data = textscan(fID,sonVar,'HeaderLines',4,...
+    'Delimiter',delim,'treatAsEmpty','"NAN"');
+if strcmp(data{1}(end),'')
+    data{1} = data{1}(1:end-1);
+end
+datesC = regexprep(data{1}, '"', '');
+dates = datenum(datesC,'yyyy-mm-dd HH:MM');
+[timeO,depthO,varO] = profilerTo2D(dates,data{zI},data{varI},zInt,zOff,tInt);
+
+var = data{varI};
+dep = data{zI};
+useI = gt(dep,0) & lt(dep,1);
+
+plot(timeO,varO);
+datetick;
+hold on;
+plot(dates(useI),var(useI),'c.')
+gFileSave(fileOut,timeO, varO, varN, depthO,'overwrite')
+end
+
---
+++ profilerTo2D.m
@@ -0,0 +1,56 @@
+function [timeO,depthO,varO] = profilerTo2D(time,depth,var,zInt,zOff,tInt)
+
+% converts 1D profiler data into 2D matrix of data
+
+% time:     vector of time points for depth and var measurements
+% depth:    vector of depth measurements
+% var:      vector of measurements of the variable to be converted to 2D
+% zInt:     scalar of interval spacing that the profiler uses
+% zOff:     scalar offset of depth interval (z = z_meas+zOff)
+% tInt:     scalar (day frac) of binning for output
+
+minPt = 3;  % min points for a depth
+mxDat = 200;
+%% convert data:
+depth = depth+zOff;             % now set near actual reading
+depth = round(depth*zInt)*zInt; % now rounded to values
+
+% ---throw out points during the rise cycle (points that aren't surrounded by
+% similar points---
+% should be increasing or staying the same, except to start new dwell at 0
+
+% --remove errant data--
+rmvI = gt(var,mxDat);
+time = time(~rmvI);
+depth= depth(~rmvI);
+var  = var(~rmvI);
+
+% --remove upward travel-- *assumes profiler is not errant
+dif = depth(2:end)-depth(1:end-1);
+dif(end+1) = 0;
+moveI = lt(dif,0)&ne(depth,max(depth));
+moveI(end+1)= false;
+depth(moveI) = [];
+time(moveI) = [];
+var(moveI) = [];
+
+% -- assign new depths -- 
+depthO = unique(depth);
+depthO = depthO(~isnan(depthO));
+stTime = ceil(time(1)/tInt)*tInt;
+timeO  = stTime:tInt:time(end);
+varO   = NaN(length(timeO),length(depthO));
+
+for i = 1:length(depthO)
+    depI = eq(depth,depthO(i));        % all matching indices for this z
+    if lt(sum(depI),minPt);
+        depthO(i) = NaN;
+    else
+        [varDs,timeDs] = downsample_interval(var(depI),time(depI),tInt*86400);
+        varO(:,i) = interp1(timeDs,varDs,timeO);
+    end
+end
+nanI = isnan(depthO);
+varO(:,nanI) = [];
+depthO(nanI) = [];
+
